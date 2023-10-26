56d5e677a727b732dfd82f3b9ce2265e24787610
Kattarinea <65298723+Kattarinea@users.noreply.github.com>
2023-10-26 19:15:56 +0300

    Add files via upload

--- preprocessing.py
+++ preprocessing.py
@@ -2,9 +2,13 @@ import pymorphy2
 import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import spacy
 
 morph = pymorphy2.MorphAnalyzer()
 stop_words = set(stopwords.words('russian'))
+
+nlp = spacy.load('en_core_web_sm')
+stopwords = nlp.Defaults.stop_words
 
 def multipreprocessing_text(text):
     filtered_text = re.sub(r'[^a-zA-Zа-яА-ЯёЁ0-9\s]', '', text)
@@ -12,3 +16,12 @@ def multipreprocessing_text(text):
     filtered_text = [word for word in word_tokens if word not in stop_words]
     lem = [morph.normal_forms(word)[0] for word in filtered_text]
     return lem
+
+def multipreprocessing_text_spacy(data):
+    result = [token.lemma_.lower() for token in nlp(data) if
+            not token.is_stop
+            and not token.is_space
+            and not token.like_num
+            and not token.is_punct
+            and not token.is_digit]
+    return result
